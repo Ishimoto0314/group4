3536d0914760616d7a0736e5b30ee319fdbcf3c1
koki-333 <84367101+koki-333@users.noreply.github.com>
2021-11-22 10:12:40 +0900

    Add files via upload

---
+++ server/cgi-bin/index.py
@@ -0,0 +1,27 @@
+#!usr/bin/python
+# -*- coding: utf-8 -*-
+
+import cgi
+import cgitb
+import sys
+
+cgitb.enable()
+
+form = cgi.FieldStrorage()
+
+
+print("Content-Type: text/html; charset=UTF-8")
+print("")
+
+
+if "text" not in form:
+	print("<h1>Error!</h1>")
+	print("<br>")
+	print("テキストを入力してください")
+	print("<a href='/'><button type='submit'>戻る</button></a>")
+	sys.exit()
+
+
+text = form.getvalue(text)
+
+print(text)
